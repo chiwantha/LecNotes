2972264c71f24085169611114c2bbdf3d8ea046f
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2026-08-03 12:21:13 +0530

    new

---
+++ Oop/20260803/ex02.java
@@ -0,0 +1,10 @@
+public class ex02 {
+    public static void main(String[] args) {
+        Double answer = Math.sin((Math.PI / 180.0)*50);
+        Double squer = Math.sqrt(5);
+        Double power = Math.pow(5, 2);
+        System.out.println("Sin 50: " + answer);
+        System.out.println("Sqrt 5: " + squer);
+        System.out.println("Power 5^2: " + power);
+    }
+}
